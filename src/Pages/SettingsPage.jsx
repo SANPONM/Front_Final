fcbbe6bf197dd40f7e5dcb157741bc011d5b7ebd
Stierlitzx <aib.lbsg@gmail.com>
2024-12-20 01:53:26 +0500

    Profile Button

---
+++ src/Pages/SettingsPage.jsx
@@ -0,0 +1,13 @@
+// SettingsPage.jsx
+import React from 'react';
+
+const SettingsPage = () => {
+  return (
+    <div>
+      <h1>Settings</h1>
+      <p>Here you can update your account settings.</p>
+    </div>
+  );
+};
+
+export default SettingsPage;
