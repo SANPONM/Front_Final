9dd9d6eaba9c42d64dc4085e3be44f81c2c5a6a5
Stierlitzx <aib.lbsg@gmail.com>
2024-12-21 04:21:11 +0500

    App

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import Header from './Header/Header.jsx'
 import Footer from './footer/Footer.jsx'
+import App from './app.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Header />
-    <Footer />
+    <App />
   </StrictMode>,
 )
